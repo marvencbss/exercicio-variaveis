0231082979bba16ad2aa0264606347bde4ae9802
marvencbss <marvencbs12@gmail.com>
2023-04-19 21:44:59 -0300

    subindo exercicios de variaveis

---
+++ exercicios.js
@@ -0,0 +1,38 @@
+//ex 1 - int. de codigo
+10
+10 5
+
+//ex 2 - int. de codigo
+20
+20
+20
+
+//ex 3 - int. de codigo
+p = HorasDia
+t = PayDia
+
+//ex 1 escrita de codigo
+let nome = toString(prompt("Digite seu nome"))
+let idade = Number(prompt("Digite sua idade"))
+console.log(typeof nome, typeof idade)
+
+//foi escrito por atribuir a categoria antes do comando "prompt"
+const nome = (prompt("Digite seu nome"))
+const idade = (prompt("Digite sua idade"))
+console.log("Olá", nome, "você tem" ,idade,"anos");
+
+//ex 2 escrita de codigo
+const doce = prompt("Você gosta de doces?")
+const dog = prompt("Você gosta de cachorros?")
+const catt = prompt("Você gosta de gatos?")
+console.log("Você gosta de doces?" ,doce)
+console.log("Você gosta de cachorros?" ,dog)
+console.log("Você gosta de gatos?" ,catt)
+
+//ex 3 escrita de codigo
+let a = 10
+let b = 25
+a = b
+b = a
+console.log("O novo valor de a é", a) //o novo valor de a é 25
+console.log("O novo valor de b é", b) //o novo valor de b é 25
